53369fea4d6736326a9841ddbe24322cd02901f6
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2021-08-01 13:29:05 -0300

    Ensure the auth use case return 401 status code when is invalid credentials

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,4 +1,5 @@
 const MissingParamError = require('../helpers/missing-param-error');
+const UnauthorizedError = require('../helpers/unauthorized-error');
 const LoginRouter = require('./login-router');
 
 function makeSut() {
@@ -60,5 +61,18 @@ describe('Login Router', () => {
     sut.route(httpRequest);
     expect(authUseCaseSpy.email).toBe(httpRequest.body.email);
     expect(authUseCaseSpy.password).toBe(httpRequest.body.password);
+  });
+
+  it('Should return 401 when invalid credentials are provided', () => {
+    const { sut } = makeSut();
+    const httpRequest = {
+      body: {
+        email: 'invalid_email@email.com',
+        password: 'invalid_password',
+      },
+    };
+    const httpResponse = sut.route(httpRequest);
+    expect(httpResponse.statusCode).toBe(401);
+    expect(httpResponse.body).toEqual(new UnauthorizedError());
   });
 });
