0c9ea87313151a3b066a77d3ff53a716db8a60ca
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2021-08-07 16:27:02 -0300

    Ensure the password wis provided

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -1,6 +1,9 @@
+const MissingParamError = require('../../utils/errors/missing-param-error');
+
 class AuthUseCase {
-  async auth(email) {
-    if (!email) throw new Error();
+  async auth(email, password) {
+    if (!email) throw new MissingParamError('email');
+    if (!password) throw new MissingParamError('password');
   }
 }
 
@@ -8,6 +11,12 @@ describe('Auth UseCase', () => {
   it('Should throw if no email is provided', async () => {
     const sut = new AuthUseCase();
     const promise = sut.auth();
-    expect(promise).rejects.toThrow();
+    expect(promise).rejects.toThrow(new MissingParamError('email'));
+  });
+
+  it('Should throw if no password is provided', async () => {
+    const sut = new AuthUseCase();
+    const promise = sut.auth('any_email@gmail.com');
+    expect(promise).rejects.toThrow(new MissingParamError('password'));
   });
 });
