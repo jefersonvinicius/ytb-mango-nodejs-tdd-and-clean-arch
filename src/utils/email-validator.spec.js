b7cc1bcb3d8728ec28bdbb00265cf33e06599382
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2021-08-07 16:07:08 -0300

    Add email validator class

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -1,13 +1,27 @@
-class EmailValidator {
-  isValid(email) {
-    return true;
-  }
+const EmailValidator = require('./email-validator');
+const validator = require('validator');
+
+function makeSut() {
+  return new EmailValidator();
 }
 
 describe('Email Validator', () => {
   it('Should return true if validator returns true', () => {
-    const sut = new EmailValidator();
+    const sut = makeSut();
     const isEmailValid = sut.isValid('valid_email@gmail.com');
-    expect(isEmailValid).toBeTruthy();
+    expect(isEmailValid).toBe(true);
+  });
+
+  it('Should return true if validator returns true', () => {
+    validator.isEmailValid = false;
+    const sut = makeSut();
+    const isEmailValid = sut.isValid('invalid_email');
+    expect(isEmailValid).toBe(false);
+  });
+
+  it('Should call validator with correct email', () => {
+    const sut = makeSut();
+    sut.isValid('any_email@gmail.com');
+    expect(validator.email).toBe('any_email@gmail.com');
   });
 });
