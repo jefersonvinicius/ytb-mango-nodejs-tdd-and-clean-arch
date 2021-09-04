b7a2ec4519b9c539ecf1d319bf210f48e68d7376
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2021-09-04 18:23:48 -0300

    Add case to ensure dependencies are provided

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -73,6 +73,12 @@ describe('Auth UseCase', () => {
     expect(loadUserByEmailRepositorySpy.email).toBe('any_email@gmail.com');
   });
 
+  it('Should throw error if no dependencies are provided', async () => {
+    const sut = new AuthUseCase();
+    const promise = sut.auth('any_email@gmail.com', 'any_password');
+    expect(promise).rejects.toThrow();
+  });
+
   it('Should throw error if no LoadUserByEmailRepository is provided', async () => {
     const sut = new AuthUseCase({});
     const promise = sut.auth('any_email@gmail.com', 'any_password');
