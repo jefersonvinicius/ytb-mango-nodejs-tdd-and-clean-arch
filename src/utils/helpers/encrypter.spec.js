05c3914b1fdc06a5f669c5abbe485c4595fea49c
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2021-10-07 08:36:36 -0300

    Ensure the Encrypter calls bcrypt with correct params

--- src/utils/helpers/encrypter.spec.js
+++ src/utils/helpers/encrypter.spec.js
@@ -14,4 +14,11 @@ describe('Encrypter', () => {
     const isValid = await sut.compare('any_value', 'hashed_value');
     expect(isValid).toBe(false);
   });
+
+  it('should call bcrypt with correct values', async () => {
+    const sut = new Encrypter();
+    await sut.compare('any_value', 'hashed_value');
+    expect(bcrypt.value).toBe('any_value');
+    expect(bcrypt.hash).toBe('hashed_value');
+  });
 });
