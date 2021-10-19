924c13d78eddb36b0ac1de35f32a2e706f1ad88a
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2021-10-19 08:48:22 -0300

    Ensure UpdateAccessTokenRepository updates user with the given accessToken

---
+++ src/infra/repositories/update-access-token-repository.spec.js
@@ -0,0 +1,43 @@
+const MongoHelper = require('../helpers/mongo-helper');
+
+class UpdateAccessTokenRepository {
+  constructor(userModel) {
+    this.userModel = userModel;
+  }
+
+  async update(userId, accessToken) {
+    await this.userModel.updateOne({ _id: userId }, { $set: { accessToken } });
+  }
+}
+
+let db;
+
+describe('UpdateAccessToken Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(global.__MONGO_URI__);
+    db = await MongoHelper.getDB();
+  });
+
+  beforeEach(async () => {
+    await db.collection('users').deleteMany();
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  it('should update the user with the given accessToken', async () => {
+    const userModel = db.collection('users');
+    const insertion = await userModel.insertOne({
+      email: 'any@gmail.com',
+      name: 'any_name',
+      password: 'hashed',
+    });
+    const sut = new UpdateAccessTokenRepository(userModel);
+
+    await sut.update(insertion.insertedId, 'valid_token');
+
+    const updatedUser = await userModel.findOne({ _id: insertion.insertedId });
+    expect(updatedUser.accessToken).toBe('valid_token');
+  });
+});
