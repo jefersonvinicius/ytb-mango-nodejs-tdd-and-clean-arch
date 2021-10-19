badff1fb27a778dac56e8a9a2e1715cc5a5ffe37
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2021-10-19 09:01:26 -0300

    Move class UpdateAccessTokenRepository to own file

--- src/infra/repositories/update-access-token-repository.spec.js
+++ src/infra/repositories/update-access-token-repository.spec.js
@@ -1,18 +1,6 @@
 const MissingParamError = require('../../utils/errors/missing-param-error');
 const MongoHelper = require('../helpers/mongo-helper');
-
-class UpdateAccessTokenRepository {
-  constructor(userModel) {
-    this.userModel = userModel;
-  }
-
-  async update(userId, accessToken) {
-    if (!userId) throw new MissingParamError('userId');
-    if (!accessToken) throw new MissingParamError('accessToken');
-
-    await this.userModel.updateOne({ _id: userId }, { $set: { accessToken } });
-  }
-}
+const UpdateAccessTokenRepository = require('./update-access-token-repository');
 
 function makeSut() {
   const userModel = db.collection('users');
