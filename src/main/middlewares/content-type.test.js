672822b1bdb0cf71e39d62f11a7ac8ee4d592349
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2021-10-20 09:30:23 -0300

    Ensure the content type is retuned correctly when is setted

--- src/main/middlewares/content-type.test.js
+++ src/main/middlewares/content-type.test.js
@@ -3,11 +3,21 @@ const app = require('../app');
 
 const request = supertest(app);
 
-describe('CORS middleware', () => {
+describe('ContentType middleware', () => {
   it('should returns content type with json as default', async () => {
     app.get('/test', (req, res) => res.send(''));
 
     const response = await request.get('/test').send();
     expect(response.headers['content-type']).toMatch(/json/);
+  });
+
+  it('should return xml content-type if forced', async () => {
+    app.get('/test_xml', (req, res) => {
+      res.set('content-type', 'text/xml');
+      return res.send('');
+    });
+
+    const response = await request.get('/test_xml').send();
+    expect(response.headers['content-type']).toMatch(/xml/);
   });
 });
