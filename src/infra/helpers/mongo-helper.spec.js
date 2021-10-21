7829ea5d1d38395848895adc0c62870eddfa4eed
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2021-10-21 08:31:24 -0300

    Disconnect mongo db after mongo helper

--- src/infra/helpers/mongo-helper.spec.js
+++ src/infra/helpers/mongo-helper.spec.js
@@ -1,12 +1,11 @@
-const MongoHelper = require('./mongo-helper');
+const sut = require('./mongo-helper');
 
 describe('MongoHelper', () => {
   afterAll(async () => {
-    await MongoHelper.disconnect();
+    await sut.disconnect();
   });
 
   it('should getDb() return a value even when database is disconnected', async () => {
-    const sut = MongoHelper;
     await sut.connect(global.__MONGO_URI__);
     expect(sut.db).toBeTruthy();
 
