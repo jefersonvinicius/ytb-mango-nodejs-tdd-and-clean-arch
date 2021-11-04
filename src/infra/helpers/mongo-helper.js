387724cd0dbae131bd8d0e299588e3f6994969f5
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2021-11-04 09:02:37 -0300

    Remove db name from mongo helper

--- src/infra/helpers/mongo-helper.js
+++ src/infra/helpers/mongo-helper.js
@@ -6,11 +6,10 @@ module.exports = {
   dbName: undefined,
   db: null,
 
-  async connect(uri, dbName) {
+  async connect(uri) {
     this.uri = uri;
-    this.dbName = dbName;
     this.client = await MongoClient.connect(uri);
-    this.db = this.client.db(dbName);
+    this.db = this.client.db();
   },
 
   async disconnect() {
@@ -21,7 +20,7 @@ module.exports = {
 
   async getCollection(name) {
     if (!this.client) {
-      await this.connect(this.uri, this.dbName);
+      await this.connect(this.uri);
     }
     return this.db.collection(name);
   },
