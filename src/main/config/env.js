c52f2e379244e939fcb4dbea5c7c4b2b4d13973c
Jeferson Vinícius <jeferson.viniciuscrc@gmail.com>
2021-11-04 09:05:29 -0300

    Fix app port

--- src/main/config/env.js
+++ src/main/config/env.js
@@ -1,3 +1,5 @@
 module.exports = {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:4343/clean-node-api',
+  tokenSecret: process.env.TOKEN_SECRET || 'secret',
+  port: process.env.PORT || 5959,
 };
--- src/main/index.js
+++ src/main/index.js
@@ -5,8 +5,8 @@ const env = require('./config/env');
 
 MongoHelper.connect(env.mongoUrl)
   .then(() => {
-    app.listen(5959, () => {
-      console.log('Server Running');
+    app.listen(env.port, () => {
+      console.log(`Server running at http://localhost:${env.port}`);
     });
   })
   .catch(console.error);
